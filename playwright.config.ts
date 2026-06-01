9cd86ebd949cd006853319b6d65ddab2f1e6a1cc
ellierst <sifid64@gmail.com>
2026-06-01 23:08:40 +0300

    Delete webkit tests, add firefox tests, update README.md. Update workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 import { env } from './utils/env';
 
 export default defineConfig({
@@ -54,14 +54,9 @@ export default defineConfig({
         },
     },
 
-    //{
-      //name: 'firefox',
-      //use: { ...devices['Desktop Firefox'] },
-    //},
-
-    //{
-      //name: 'webkit',
-      //use: { ...devices['Desktop Safari'] },
-    //},
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
   ],
 });
